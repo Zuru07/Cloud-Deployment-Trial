28d664588c6dd3d02527c0d46feb8e963300ad7d
Zuru07 <suraj.manivannan@gmail.com>
2025-11-03 10:58:10 +0530

    App changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from GCP Cloud Run! CI/CD works!</h1>');
+  res.send('<h1>Hello from GCP Cloud Run! CI/CD works! But does it though?</h1>');
 });
 
 app.listen(PORT, () => {
